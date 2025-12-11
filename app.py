55c7f050743a23a6493efe9d78a2e6e9c10aa649
darikandro <kanddaria@gmail.com>
2025-12-11 18:14:02 +0300

    tasks_pages

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 from flask import Flask, render_template
 from sqlalchemy import create_engine
 from models import Tasks, Topics
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session
+from db_setup import db, migrate
 
-engine = create_engine("mysql+pymysql://root:fk_kbkEz33@localhost/math")
-engine.connect()
+def main():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:fk_kbkEz33@localhost/math"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app = Flask(__name__)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/problems/')
-def problems_list():
+    @app.route('/problems/')
+    def problems_list():
+        tasks = Tasks.query.all()
+        return render_template('problems.html', tasks=tasks)
 
-    session = Session(bind=engine)
-    tasks = session.query(Tasks).all()
-
-    return render_template('problems.html', tasks=tasks)
-
-@app.route('/problems/topic/<topic_id>')
-def problems_list_for_topics(topic_id):
+    @app.route('/problems/topic/<topic_id>')
+    def problems_list_for_topics(topic_id):
+        tasks = Tasks.query.filter(Tasks.topic_id == topic_id).all()
+        return render_template('problems.html', tasks=tasks)
     
-    session = Session(bind=engine)
-    tasks = session.query(Tasks).filter(Tasks.topic_id == topic_id).all()
-
-    return render_template('problems.html', tasks=tasks)
+    @app.route('/problems/task/<int:task_id>')
+    def show_task(task_id):
+        task = Tasks.query.get(task_id)
+        return render_template('task.html', task=task)
+    
+    return app
 
 if __name__ == '__main__':
+    app = main()
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,28 +1,23 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from db_setup import db
 
-Base = declarative_base()
-
-class Topics(Base):
+class Topics(db.Model):
     __tablename__ = 'topics'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
 
     def __str__(self):
         return self.name
 
-class Tasks(Base):
+class Tasks(db.Model):
     __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(200))
-    description = Column(String(1000))
-    difficulty_label = Column(String(20))
-    difficulty_score = Column(Integer)
-    topic_id = Column(Integer, ForeignKey('topics.id'))
-    created_at = Column(DateTime)
-    answer_type = Column(String(20))
-    answer = Column(String(50))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200))
+    description = db.Column(db.String(1000))
+    difficulty_label = db.Column(db.String(20))
+    difficulty_score = db.Column(db.Integer)
+    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+    created_at = db.Column(db.DateTime)
+    answer_type = db.Column(db.String(20))
+    answer = db.Column(db.String(50))
 
-    topic = relationship(Topics)
-
+    topic = db.relationship(Topics)
